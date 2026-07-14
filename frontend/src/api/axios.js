2a9caee0ed004d4d9d78f68d250700899037176b
Atharv Verma <verma.atharv@gmail.com>
2026-07-14 14:50:29 +0530

    Update API URLs for production deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://farmfresh-backend-j2n4.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
